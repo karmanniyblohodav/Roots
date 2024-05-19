dcb0387e5646ed1b7ae59f5cc74a6d97ca4fdf70
karmanniyblohodav <artt.lopatin2015@yandex.ru>
2024-05-19 20:02:19 +0300

    Написаны тесты. Добалена документация.

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
 #include <cmath>
+#include <exception>
 #include "..\Solver\DichotomyMethod.h"
 #include "..\Solver\BruteForceMethod.h"
 #include "..\Solver\Solver.h"
 using namespace std;
 
+/**
+* @brief Точка входа в программу
+* @return 0 в случае успеха
+*/
 int main() {
-	
-	auto func = [](const double x) { return x*x-4;  };
-	auto leftBound = 0;
-	auto rightBound = 10;
-	auto step = 1;
-	auto epsilon = 0.000001;
-	auto root1 = new DichotomyMethod();
-	auto root2 = new BruteForceMethod();
+    auto func = [](const double x) { return x * x - 4; };
+    double leftBound = 0;
+    double rightBound = -10;
+    double step = 1;
+    double epsilon = 0.000001;
+    auto root1 = new DichotomyMethod();
+    auto root2 = new BruteForceMethod();
 
-	cout << root1->GetValue(leftBound, rightBound, step, epsilon, func) << endl;
-	cout << root2->GetValue(leftBound, rightBound, step, func) << endl;
-	
-	return 0;
+    try {
+        cout << root1->GetValue(leftBound, rightBound, step, epsilon, func) << endl;
+        cout << root2->GetValue(leftBound, rightBound, step, func) << endl;
+    }
+    catch (const std::logic_error& e) {
+        cerr << e.what() << endl;
+    }
+
+    return 0;
 }
---
+++ Solver.Tests/BruteForceMethod.cpp
@@ -0,0 +1,57 @@
+﻿#include "CppUnitTest.h"
+#include "..\Solver\BruteForceMethod.h"
+#include <functional>
+#include <stdexcept>
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SolverTests
+{
+    TEST_CLASS(BruteForceMethodTests)
+    {
+    public:
+
+        TEST_METHOD(TestRootFindingForQuadratic)
+        {
+            auto func = [](const double x) { return x * x - 4; }; // Roots: -2 and 2
+            double leftBound = 0;
+            double rightBound = 3;
+            double step = 0.01;
+
+            BruteForceMethod solver;
+            double result = solver.GetValue(leftBound, rightBound, step, func);
+
+            // Verify if result is close to 2 (considering small numerical inaccuracies)
+            Assert::IsTrue(std::abs(result - 2) < 0.01);
+        }
+
+        TEST_METHOD(TestExceptionForInvalidBounds)
+        {
+            auto func = [](const double x) { return x * x - 4; };
+            double leftBound = 3;
+            double rightBound = 0;
+            double step = 0.01;
+
+            BruteForceMethod solver;
+
+            // Verify if exception is thrown for invalid bounds
+            auto funcToTest = [&]() { solver.GetValue(leftBound, rightBound, step, func); };
+            Assert::ExpectException<std::logic_error>(funcToTest);
+        }
+
+        TEST_METHOD(TestExceptionForInvalidStep)
+        {
+            auto func = [](const double x) { return x * x - 4; };
+            double leftBound = 0;
+            double rightBound = 3;
+            double step = -0.01;
+
+            BruteForceMethod solver;
+
+            // Verify if exception is thrown for invalid step
+            auto funcToTest = [&]() { solver.GetValue(leftBound, rightBound, step, func); };
+            Assert::ExpectException<std::logic_error>(funcToTest);
+        }
+    };
+}
